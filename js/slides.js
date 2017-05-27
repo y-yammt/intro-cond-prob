5cf42a611a36c7c8c7838b476035ae03696e08eb
y_yammt <y.yammt@gmail.com>
2017-05-27 14:33:31 +0900

    Update slides.js

--- js/slides.js
+++ js/slides.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     // Note the `delayStartupUntil=configured` parameter
-    mathjax: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM-MML_HTMLorMML&amp;delayStartupUntil=configured',
+    mathjax: 'http://cdnjs.cloudflare.com/mathjax/latest/MathJax.js?config=TeX-AM-MML_HTMLorMML&amp;delayStartupUntil=configured',
     jquery: 'jquery-2.1.1.min'
   },
   shim: {
